dae1dbfe3cbe864259097c01e2a7814c9713e593
Simon Chervenak <sarras305@gmail.com>
2024-09-16 16:10:54 -0400

    citying

--- new-game-project/city.gd
+++ new-game-project/city.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+var city: String
+var X: int
+var Y: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	position.x = X
+	position.y = Y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- new-game-project/map_maker.gd
+++ new-game-project/map_maker.gd
@@ -1,9 +1,30 @@
 extends Node
 
+@export var city_scene: PackedScene = preload("res://city.tscn")
+var map = "res://maps/map1.txt"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var file = FileAccess.open(map, FileAccess.READ)
+	var content = file.get_as_text()
+	
+	var citying = true
+	for line in content.split("\n"):
+		if !line:
+			citying = false
+			continue
+		
+		if citying:
+			var city = line.split(",")
+			
+			var cityNode = city_scene.instantiate()
+			cityNode.city = city[0]
+			cityNode.X = city[1]
+			cityNode.Y = city[2]
+			
+			add_child(cityNode)
+		else:
+			var train = line.split(",")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
