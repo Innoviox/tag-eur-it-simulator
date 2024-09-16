403968dcb3d2f6e64127c2d394b9bf9e634b5dd5
Simon Chervenak <sarras305@gmail.com>
2024-09-16 16:33:45 -0400

    add api link

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Start goal
 
 - have a network of 5x5 nodes display in the scene
 - user is at a current node and 
+
+
+
+https://reiseauskunft.bahn.de/bin/bhftafel.exe/en/dn?input=8000001&boardType=dep&time=actual&productsDefault=1111101&start=yes
--- new-game-project/map_maker.gd
+++ new-game-project/map_maker.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @export var city_scene: PackedScene = preload("res://city.tscn")
+@export var route_scene: PackedScene = preload("res://route.tscn")
+
 var map = "res://maps/map1.txt"
 
 # Called when the node enters the scene tree for the first time.
@@ -25,6 +27,9 @@ func _ready() -> void:
 			add_child(cityNode)
 		else:
 			var train = line.split(",")
+			
+			var trainNode = route_scene.instantiate()
+			
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
